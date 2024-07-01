85523aec5f0fa5168e2407a03c0546a93d278e65
Justin Elmore <jelmore1674@pm.me>
2024-07-01 08:22:10 -0500

    hide project 2 warning and start keymaps

--- lua/justin/lazy/octo.lua
+++ lua/justin/lazy/octo.lua
@@ -1,3 +1,7 @@
+local keymap = function(key, cmd, desc)
+  vim.keymap.set('n', key, cmd, { noremap = true, silent = true, desc = desc })
+end
+
 return {
   'pwntester/octo.nvim',
   requires = {
@@ -7,7 +11,10 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('octo').setup({ enable_builtin = true })
+    require('octo').setup({ enable_builtin = true, suppress_missing_scope = {
+      projects_v2 = true,
+    } })
     -- TODO: Configure Keybinds
+    keymap('<leader>opl', '<Cmd>Octo pr list<CR>', '[O]cto [P]ull Request [L]ist ')
   end,
 }
